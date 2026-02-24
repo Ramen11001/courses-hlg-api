e516360509e7fb4da934fb3411b6e084327a273b
Ramen11001 <soltashnd@gmail.com>
2026-02-24 23:26:52 +0100

    [FTR] - Add new const mssg #12

--- src/const/messages.const.js
+++ src/const/messages.const.js
@@ -8,4 +8,7 @@ export const messageValidator = {
   DUPLICATE_USER_PHONES: "No puede tener números de teléfono duplicados",
   VALIDITY_YEAR:
     "Los usuarios deben tener más de 12 años. La fecha debe ser anterior al año 2014",
+  BIRTH_DAY_MESSAGE: "Feliz cumpleaños!!",
+  WELCOME_MESSAGE:
+    "Bienvenido al caálogo de cursos de Holguín. Feliz viaje entrelos caminos del saber!",
 };
