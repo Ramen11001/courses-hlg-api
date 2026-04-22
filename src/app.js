250827aff16677119ad5a991106183dfb117c5dc
Ramen11001 <soltashnd@gmail.com>
2026-04-22 22:07:34 +0200

    [FIX] - Fix route bugs in app

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var coursesRouter = require("./routes/courses");
-var commentsRouter = require("./routes/comments"); //wtf
+var commentsRouter = require("./routes/comments"); 
 const authRouter = require("./routes/auth");
 var app = express();
 
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/auth", authRouter);
-app.use("/users", usersRouter);
+app.use("/user", usersRouter);
 app.use("/courses", coursesRouter);
 app.use("/comments", commentsRouter);
 // catch 404 and forward to error handler
@@ -46,9 +46,8 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  // return JSON error for API
+  res.status(err.status || 500).json({ error: err.message });
 });
 
 module.exports = app;
