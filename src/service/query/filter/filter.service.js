aedc2698f036caa50f49afb012cad60e0b4c673a
Ramen11001 <soltashnd@gmail.com>
2026-04-27 01:05:20 +0200

    [FIX] - Fix filter price #12

--- src/service/query/filter/filter.service.js
+++ src/service/query/filter/filter.service.js
@@ -32,14 +32,14 @@ function filterPagination(req, res, next) {
   if (req.query.minPrice && !isNaN(req.query.minPrice)) {
     // Add price filter for minimum cost
     priceFilters.push({
-      price: { [Op.gte]: parseFloat(req.query.minPrice) },
+      cost: { [Op.gte]: parseFloat(req.query.minPrice) },
     });
   }
 
   if (req.query.maxPrice && !isNaN(req.query.maxPrice)) {
     // Add price filter for maximum price
     priceFilters.push({
-      price: { [Op.lte]: parseFloat(req.query.maxPrice) },
+      cost: { [Op.lte]: parseFloat(req.query.maxPrice) },
     });
   }
 
